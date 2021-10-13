d17f9df50366de0c328a990328e658953a07dded
Sandi <sandis@ebe-inc.com>
2021-10-14 01:16:02 +0200

    created test data

--- angular-cva/apps/registration-app/src/app/containers/registration/registration.component.ts
+++ angular-cva/apps/registration-app/src/app/containers/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Registration } from '../../models';
 
 @Component({
   selector: 'angular-cva-registration',
@@ -8,6 +9,21 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
   constructor() {}
+
+  registrationFormData: Registration = {
+    firstName: 'Milivoj',
+    lastName: 'Savic',
+    primaryAddress: {
+      addressLine: 'Butori 7, Tinjan',
+      country: 'Canada',
+      state: 'test',
+    },
+    secondaryAddress: {
+      addressLine: 'Test 7, Tinjan',
+      country: 'USA',
+      state: 'NY',
+    },
+  };
 
   ngOnInit(): void {}
 }
